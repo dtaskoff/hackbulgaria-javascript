8fce5937312a6bbcb4692d3486bff1991a2ba7bf
dtaskoff <danko@abv.bg>
2014-06-04 23:25:32 +0300

    Adding pluck, zip and nestedLists problems

---
+++ week1/zip-test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+var zip = require('./zip').zip;
+
+exports.testZip = function (test) {
+    test.deepEqual([[1, 4], [2, 5], [3, 6]], zip([1, 2, 3], [4, 5, 6]));
+    test.done();
+};
+
+exports.testZip = function (test) {
+    test.deepEqual([[1, 4], [2, 5], [3, undefined]], zip([1, 2, 3], [4, 5]));
+    test.done();
+};
