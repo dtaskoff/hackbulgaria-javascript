827343cac81e0a2120ae54a207bb8c27db5bbfe6
dtaskoff <danko@abv.bg>
2014-07-22 20:21:03 +0300

    Changed method to use hashtable instead of js object

--- week6/exam2/1-Corrupted-Data/app.js
+++ week6/exam2/1-Corrupted-Data/app.js
@@ -2,31 +2,27 @@
 
 
 var data = require('./data.json');
+var HashTable = require('hashtable');
 
 
 var getRepeatableCheckins = function (checkins) {
-    var occurrencesOfCheckin  = {},
+    var occurrencesOfCheckin  = new HashTable(),
         repeatableCheckins = [];
 
     checkins.forEach(function (checkin) {
-        var hashedCheckin = hash(checkin);
-
         // only adding checkin to repeatableCheckins,
         // if it occurs for a second time,
         // else just increment it's occurrences
-        if (occurrencesOfCheckin[hashedCheckin] === undefined) {
-            occurrencesOfCheckin[hashedCheckin] = 0;
-        } else if (occurrencesOfCheckin[hashedCheckin] === 1) {
+        if (occurrencesOfCheckin.get(checkin) === undefined) {
+            occurrencesOfCheckin.put(checkin, 0);
+        } else if (occurrencesOfCheckin.get(checkin) === 1) {
             repeatableCheckins.push(checkin);
         }
-        occurrencesOfCheckin[hashedCheckin] += 1;
+        occurrencesOfCheckin.put(checkin,
+            occurrencesOfCheckin.get(checkin) + 1);
     });
 
     return repeatableCheckins;
-};
-
-var hash = function (object) {
-    return object.toString();
 };
 
 console.log(getRepeatableCheckins(data));
